2a2a4a4c8fc6eed07ee4e6ab5c7ba6fa012d15e3
easy <aaa002934@gmail.com>
2024-11-16 18:01:38 +0300

    5 шаг

--- src/main/java/DeleteServlet.java
+++ src/main/java/DeleteServlet.java
@@ -1,41 +1,19 @@
-
+import java.io.IOException;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * Servlet implementation class DeleteServlet
- */
-@WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+                                    throws ServletException, IOException {
+        for(String id : req.getParameterValues("id")) {
+            try {
+                Storage.delete(Integer.parseInt(id));
+            } catch(NumberFormatException e) {}
+        }
+        resp.sendRedirect(req.getContextPath() + "/index.html");
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
